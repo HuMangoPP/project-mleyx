05da8252c1dc26fa880ccf1d3e0d24b83670d090
Mike Lee <49696312+HuMangoPP@users.noreply.github.com>
2024-03-03 10:25:11 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/', async (req, res) => {
 app.post('/new', async (req, res) => {
     const conn = await sql.connect(config)
     // const response = await conn.request().query(`INSERT INTO dbo.Listings (id, name, about, timestamp, image) VALUES ('${req.body.id}', '${req.body.name}', '${req.body.about}', '${req.body.timestamp}', 'file.png')`)
-    
+    const response = await conn.request().query(`INSERT INTO dbo.Listings (id, name, about) VALUES ('${req.body.id}', '${req.body.name}', '${req.body.about}')`)
     // fs.readFile(`./uploads/${req.file.filename}`, async (err, data) => {
     //     const buffer = Buffer.from(data, 'base64')
     //     const blockBlobClient = containerClient.getBlockBlobClient(req.file.filename)
